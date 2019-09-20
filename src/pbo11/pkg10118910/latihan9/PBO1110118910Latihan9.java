c9c1a248c86d4ef19a7638d6051b49cea6cdf8cf
ADMIN <ADMIN@ADMIN-PC>
2019-09-20 23:29:39 +0700

    Latihan-Java

---
+++ src/pbo11/pkg10118910/latihan9/PBO1110118910Latihan9.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo11.pkg10118910.latihan9;
+
+/**
+ *
+ * @author
+ * NAMA  : Muhamad Syifa Amrulloh 
+ * KELAS : PBO11K 
+ * NIM   : 10118910
+ *
+ * Description Program : Program ini Berisi pengunaan Teks
+ */
+public class PBO1110118910Latihan9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        char c;
+        char tab ='\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil" + newline + "Char: " + tab + "berisi " + c);
+    }
+    
+}
